98a87c6c36b979a4d064b88cbad7fe66b150e693
poliana-dev <m.polisys02@gmail.com>
2023-03-07 11:44:04 -0300

    teste para idade e genero juntos

--- verificar-idade/js/arquivo.js
+++ verificar-idade/js/arquivo.js
@@ -14,19 +14,37 @@ function verificar(){
     //teste para o usuario não inserir ano de nascimento maior que o ano atual e ano de nascimento menor que zero
     if(anoNasc>anoAtual || anoNasc<=0){
         res.innerHTML='Ano de nascimento <strong>INVÁLIDO</strong>.<br>Verifique se você preencheu os dados corretamente.'
+    //masculino
     }else if(genero[0].checked){
         if(idade>=4 && idade<=13){
             genero= 'garotinho'
         }else if(idade>=14 && idade<=19){
             genero= 'rapaz'
         }else if(idade>=20 && idade<=59){
-            
+            genero= 'homem'
+        }else if(idade<4){
+            genero= 'bebê'
+        }else{
+            genero= 'idoso'
         }
         res.innerHTML=`Você é um ${genero} de ${idade} anos`
+    //feminino
     }else{
-        genero= 'mulher'
-        res.innerHTML= `Você é uma ${genero} de ${idade} anos`
+        if (idade<4){
+            genero= 'bebê'
+        }else if(idade>=4 && idade<=13){
+            genero= 'garotinha'
+        }else if(idade>=14 && idade<=19){
+            genero= 'moça'
+        }else if(idade>=20 && idade<=59){
+            genero= 'mulher'
+        }else{
+            genero= 'idosa'
+        }
+        res.innerHTML=`Você é uma ${genero} de ${idade} anos`
+        
     }
+    
     
     
 
