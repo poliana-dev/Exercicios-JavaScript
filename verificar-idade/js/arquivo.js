187aad88b6cf87b5204ef3aa6b7642bd35676f0c
poliana-dev <m.polisys02@gmail.com>
2023-03-07 11:33:09 -0300

    testes para idade
    dependendo da idade, a variável genero recebe um termo

--- verificar-idade/js/arquivo.js
+++ verificar-idade/js/arquivo.js
@@ -6,10 +6,29 @@ function verificar(){
     var anoNascimento= document.getElementById('anoNasc'); //pegou o ano inserido pelo usuário
     var anoNasc= Number(anoNascimento.value) //convertendo para numero
     var res= document.querySelector('article#resultado');
+    var genero= document.getElementsByName('sexo');
+    var img = document.getElementById('foto')
+   
+    var idade= anoAtual-anoNasc
 
-    if(anoNasc>anoAtual || anoNasc<0){
-        res.innerHTML='Ano de nascimento <strong>INVÁLIDO</strong>'
+    //teste para o usuario não inserir ano de nascimento maior que o ano atual e ano de nascimento menor que zero
+    if(anoNasc>anoAtual || anoNasc<=0){
+        res.innerHTML='Ano de nascimento <strong>INVÁLIDO</strong>.<br>Verifique se você preencheu os dados corretamente.'
+    }else if(genero[0].checked){
+        if(idade>=4 && idade<=13){
+            genero= 'garotinho'
+        }else if(idade>=14 && idade<=19){
+            genero= 'rapaz'
+        }else if(idade>=20 && idade<=59){
+            
+        }
+        res.innerHTML=`Você é um ${genero} de ${idade} anos`
+    }else{
+        genero= 'mulher'
+        res.innerHTML= `Você é uma ${genero} de ${idade} anos`
     }
     
+    
+
 
 }
